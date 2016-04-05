5fa2bdcbafdadb175a28539008cbdb030771a0c8
Peter Henell <mail@peterhenell.se>
2016-04-05 16:51:49 +0200

    adding test as a TODO

--- performance-collector/test/stats_collector_tests/__init__.py
+++ performance-collector/test/stats_collector_tests/__init__.py
@@ -62,6 +62,8 @@ class StatsCollectorTests(unittest.TestCase):
         stat_collectors = StatCollector.from_config_manager(config_manager, queries, MockElasticSearchAPI, MockDb)
         self.assertEquals(len(stat_collectors), 2)
 
+    def test_should_crate_statsCollector_from_config_with_different_collectorClasses(self):
+        self.fail('Not implemented, issue 1')
 
 if __name__ == '__main__':
     unittest.main()
